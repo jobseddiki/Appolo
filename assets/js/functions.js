30391c3b7d164d48330c5448db56b69d3f952f6b
Zoubida SEDDIKI <JOBSEDDIKI@gmail.com>
2023-02-28 16:58:18 -0500

    HTML, JS, CSS modofier pour la section menu

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {
+/* Bouton Menu toogle  */
 	
 function nav(){
 	
@@ -12,4 +12,27 @@ function nav(){
 
 nav();
 
+//});
+
+/* balayage fluide  */
+
+$('a[href^="#"]').on('click', function(e) {
+  e.preventDefault();
+  var target = this.hash;
+  var $target = $(target);
+  $('html, body').stop().animate({
+    'scrollTop': $target.offset().top
+  }, 900, 'swing', function() {
+    window.location.hash = target;
+  });
+});
+
+/*Fixer le menu en haut de page  */
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 100) {
+    document.getElementById('menu-bar').style.position = 'fixed';
+    document.getElementById('menu-bar').style.top = '0px';
+  } else {
+    document.getElementById('menu-bar').style.position = 'static';
+  }
 });
